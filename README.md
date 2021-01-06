806f7062d48b66da8e735eb973adcfdf0459df49
maigre <thomas.bohl@gmail.com>
2021-01-06 11:21:38 +0100

    flickity

--- README.md
+++ README.md
@@ -5,3 +5,7 @@
 - enable inactivity timer only if not on first page already
 - hide pointer (show it after mouse move)
 - test touch
+- margins between images ?
+- touch zone instead of swipe ?
+
+
--- www/scripts.js
+++ www/scripts.js
@@ -7,13 +7,36 @@ socket.on('list', function(data) {
         console.log(m)
         if (m[1] == 'image') {
             var img = $('<img />').addClass("media").attr('src', '/media/' + m[0])
-            $('<div>').append(img).appendTo('#alldivs')
+            $('<div>').addClass('carousel-cell').append(img).appendTo('#alldivs')
+        } else if (m[1] == 'video') {
+            var video = $('<video>').addClass("media")
+            $('<source>').attr('src', '/media/' + m[0]).attr('type', 'video/mp4').appendTo(video)
+            $('<div>').addClass('carousel-cell').append(video).appendTo('#alldivs')
         }
 
     }
 
-    $('#alldivs').slick({
-        infinite: false,
+    // $('#alldivs').slick({
+    //     infinite: false,
+    // });
+    var carousel = $('.main-carousel').flickity({
+        // options
+        cellAlign: 'left',
+        pageDots: true,
+        contain: true,
+        selectedAttraction: 0.1,
+        friction: 0.8,
+        // fade: true
+    });
+
+    carousel.on('change.flickity', function(event, index) {
+        $('video').each((i, d) => {
+            d.pause()
+        })
+        $('.carousel-cell.is-selected').find('video').each((i, d) => {
+            console.log(d)
+            d.play()
+        })
     });
 
 });
